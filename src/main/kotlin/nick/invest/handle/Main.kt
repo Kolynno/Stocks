a81610216d172a565a629d85252159b345fb164e
Nickolay Zhmakin <nickzhmakin@mail.ru>
2024-01-20 00:00:23 +0300

    handle 2

--- src/main/kotlin/nick/invest/handle/Main.kt
+++ src/main/kotlin/nick/invest/handle/Main.kt
@@ -1,5 +1,6 @@
 package nick.invest.handle
 import java.io.File
+import java.lang.Math.abs
 
 fun main() {
 
@@ -24,34 +25,53 @@ fun main() {
             }
         }
 
-        val timeData = mutableMapOf<String, Double>()
+        val averageDays = getAVGDays(ticker)
+
+        var counter = 0
+        var daysBetweenList = mutableListOf<Int>()
 
         for (d in 1 until dataList.size) {
+
             val closeCurrent = dataList[d].second
             val closeBefore = dataList[d-1].second
             val timeCurrent = dataList[d].first
             val percent = (closeCurrent/closeBefore - 1) * 100
-            if (timeData[timeCurrent] == null) {
-                timeData[timeCurrent] = percent
-            } else {
-                var old = timeData[timeCurrent]!!
-                var new = old + percent
-                timeData[timeCurrent] = new
+            if (timeCurrent == "10:00:00") {
+                counter++
+          if (ticker == "GTRK") {
+                println(ticker +";"+ String.format("%.2f", percent))
+            }
+                if (percent >= 10) {
+                    daysBetweenList.add(kotlin.math.abs(counter - averageDays))
+                    counter = 0
+                }
             }
         }
-
-        timeData.toSortedMap()
-
-        for (data in timeData) {
-            totalData.add(Triple(ticker, data.key.substring(0,5), data.value))
+        if (counter != 0) {
+            daysBetweenList.add(kotlin.math.abs(counter - averageDays))
         }
-
+        totalData.add(Triple(ticker, "10" ,daysBetweenList.average()))
     }
+
 
         var outfile = File("E:\\Dowload\\Stocks2\\output.csv")
         for (triple in totalData) {
-            outfile.appendText("${triple.first};${triple.second};${triple.third}\n")
+            outfile.appendText("${triple.first};${String.format("%.2f",triple.third)};${triple.second}\n")
         }
 
+}
+
+fun getAVGDays(ticker: String): Int {
+    val filePath = "src/main/kotlin/nick/invest/handle/data/percent10.csv"
+    var days = 0
+    File(filePath).forEachLine { line ->
+        val tokens = line.split(";")
+        if (tokens.size == 3) {
+            if (tokens[0] == ticker) {
+                days = tokens[1].replace(",",".").toDouble().toInt()
+            }
+        }
+    }
+    return days
 }
 
